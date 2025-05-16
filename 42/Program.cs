80fe96633089affd3010192ea1e6ab5573de5abf
Ku4erenko15 <159886914+Ku4erenko15@users.noreply.github.com>
2025-05-16 12:13:57 +0300

    Add files via upload

---
+++ 42/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _42
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ten counter = new Ten();
+            Handler1 handler1 = new Handler1();
+            Handler2 handler2 = new Handler2();
+            counter.Count();
+            counter.onCount += handler1.Message;
+            counter.onCount += handler2.Message;
+            counter.onCount += counter.Message;
+            counter.Count();
+            Console.ReadKey();
+        }
+    }
+}
+
+
+ 
+
---
+++ 42/Ten.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _42
+{
+    internal class Ten
+    {
+       public delegate void CountHandler();
+        public event CountHandler onCount; // событие, которое будет вызываться при достижении 7
+
+        public void Count() 
+        { 
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+                if ((i == 7)&& (onCount != null))
+                {
+                    onCount();
+                }
+            }
+        }
+        
+        public void Message()
+        {
+            Console.WriteLine("У себя 7!");
+        }
+
+    }
+}
